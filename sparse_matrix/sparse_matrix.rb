5a8f2aa46ab5541f8cb002aa8e20f6e5913bb29f
Jacob Viau <jviau@ualberta.ca>
2014-09-17 22:53:40 -0600

    SparseMatrix.to_a added.
    Its better than the get_value iteration used previously. Could
    be better, but I'll pass on being that much of a try-hard.

--- sparse_matrix/sparse_matrix.rb
+++ sparse_matrix/sparse_matrix.rb
@@ -4,10 +4,10 @@ class SparseMatrix < Matrix
 
 	include ExceptionForMatrix
 
-	def initialize(values, columns, row_index, col_count)
+	def initialize(values, columns, row_ptrs, col_count)
 		@values = values
 		@columns = columns
-		@row_index = row_index
+		@row_ptrs = row_ptrs
 		@col_count = col_count
 	end
 
@@ -17,7 +17,7 @@ class SparseMatrix < Matrix
 		size = (rows[0] || []).size
 		values = []
 		columns = []
-		row_index = []
+		row_ptrs = []
 
 		rows.each_with_index do |x,i|
 			raise ErrDimensionMismatch, "row size differs (#{x.size} should be #{size})" unless x.size == size
@@ -29,14 +29,48 @@ class SparseMatrix < Matrix
 					first = values.size - 1 if first == -1
 				end
 			end
-			row_index << first
+			row_ptrs << first
 		end
-		row_index << values.size
-		new values, columns, row_index, size
+		row_ptrs << values.size
+		new values, columns, row_ptrs, size
+	end
+
+	def self.each(which = :all)
+		return to_enum :each, which unless block_given?
+
+		case which
+		when :all
+			block = Proc.new
+			to_a.each do |row|
+				row.each(&block)
+			end
+		else
+			raise ArgumentError, "expected #{which.inspect} to be one of :all, :diagonal, :off_diagonal, :lower, :strict_lower, :strict_upper, :upper, or :non_zeros "
+		end
+	end
+
+	def square?
+		row_count == col_count
+	end
+
+	def values
+		Vector.elements(@values)
+	end
+
+	def columns
+		Vector.elements(@columns)
+	end
+
+	def row_ptrs
+		Vector.elements(@row_ptrs)
+	end
+
+	def row_count
+		@row_ptrs.size - 1
 	end
 
 	def inspect
-		"#{self.class}[Values#{@values.inspect}Columns#{@columns.inspect}RowIndex#{@row_index.inspect}]"
+		"#{self.class}[Values#{@values.inspect}Columns#{@columns.inspect}RowIndex#{@row_ptrs.inspect}]"
 	end
 
 	def to_s
@@ -44,49 +78,49 @@ class SparseMatrix < Matrix
 	end
 
 	def to_m
-		Matrix.build(row_count, col_count) { |row, col|
-			get_value(row, col)
-		}
+		Matrix.rows(to_a)
 	end
 
-	def values
-		Vector[@values]
-	end
+	def to_a
+		a = Array.new(row_count) { Array.new(col_count) { 0 }}
+		
+		@row_ptrs[0..@row_ptrs.size - 2].each_with_index do |row_ptr_min, row|
+			next if row_ptr_min < 0
+			row_ptr_max = get_next_row_ptr(row)
 
-	def columns
-		Vector[@columns]
-	end
-
-	def row_index
-		Vector[@row_index]
-	end
-
-	def row_count
-		@row_index.size - 1
+			(row_ptr_min..row_ptr_max - 1).step(1) do |i|
+				a[row][@columns[i]] = @values[i]
+			end 
+		end
+		a
 	end
 
 	attr_reader :col_count
 
 	private
-	attr_writer :values, :columns, :row_index, :col_count
+	attr_writer :values, :columns, :row_ptrs, :col_count
 
 	def get_value(row, col)
-		row_ptr_min = @row_index[row]
-		return 0 if row_ptr_min == -1
+		row_ptr_min = @row_ptrs[row]
+		return 0 if row_ptr_min < 0
 
-		i = 1
-		row_ptr_max = @row_index[row + i]
-
-		while row_ptr_max == -1
-			i += 1
-			row_ptr_max = @row_index[row + i]
-		end
+		row_ptr_max = get_next_row_ptr(row)
 
 		(row_ptr_min...row_ptr_max).each do |i|
 			return @values[i] if @columns[i] == col
 		end
 
 		return 0
+	end
+
+	def get_next_row_ptr(index)
+		i = 1
+		next_ptr = @row_ptrs[index + i]
+		unless next_ptr >= 0
+			i += 1
+			next_ptr = @row_ptrs[index + i]
+		end
+		next_ptr
 	end
 
 end
