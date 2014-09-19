0f04ce4d2d8421fc0710781daba8317052124ac4
Jacob Viau <jviau@ualberta.ca>
2014-09-18 23:37:44 -0600

    More spizzazz added to SparseMatrix

--- sparse_matrix/sparse_matrix.rb
+++ sparse_matrix/sparse_matrix.rb
@@ -15,27 +15,22 @@ class SparseMatrix < Matrix
 		rows = convert_to_array(rows)
 		rows.map! { |row| convert_to_array(row, copy) }
 		size = (rows[0] || []).size
-		values = []
-		columns = []
-		row_ptrs = []
 
-		rows.each_with_index do |x,i|
-			raise ErrDimensionMismatch, "row size differs (#{x.size} should be #{size})" unless x.size == size
-			first = -1
-			x.each_with_index do |y,j|
-				if y != 0
-					values << y
-					columns << j
-					first = values.size - 1 if first == -1
-				end
-			end
-			row_ptrs << first
+		create(rows.size, size) do |i,j| 
+			raise ErrDimensionMismatch, "row size differs (#{x.size} should be #{size})" unless rows[i].size == size
+			rows[i][j]
 		end
-		row_ptrs << values.size
-		new values, columns, row_ptrs, size
 	end
 
-	def self.each(which = :all)
+	def self.build(row_count, column_count = row_count)
+		row_count = Matrix::CoercionHelper.coerce_to_int(row_count)
+		column_count = Matrix::CoercionHelper.coerce_to_int(column_count)
+		return to_enum :build, row_count, column_count unless block_given?
+		self.create(row_count, column_count, &Proc.new)
+	end
+
+
+	def each(which = :all)
 		return to_enum :each, which unless block_given?
 
 		case which
@@ -47,6 +42,19 @@ class SparseMatrix < Matrix
 		else
 			raise ArgumentError, "expected #{which.inspect} to be one of :all, :diagonal, :off_diagonal, :lower, :strict_lower, :strict_upper, :upper, or :non_zeros "
 		end
+	end
+
+	def row(i, &block)
+		if block_given?
+			row = get_row(i) { return self }.each(&block)
+			self
+		else
+			Vector.elements(get_row(i) { return nil })
+		end
+	end
+
+	def row_vectors
+		get_arrays { |arr| Vector.elements(arr) }
 	end
 
 	def square?
@@ -66,7 +74,7 @@ class SparseMatrix < Matrix
 	end
 
 	def row_count
-		@row_ptrs.size - 1
+		@row_ptrs.size
 	end
 
 	def inspect
@@ -82,17 +90,7 @@ class SparseMatrix < Matrix
 	end
 
 	def to_a
-		a = Array.new(row_count) { Array.new(col_count) { 0 }}
-		
-		@row_ptrs[0..@row_ptrs.size - 2].each_with_index do |row_ptr_min, row|
-			next if row_ptr_min < 0
-			row_ptr_max = get_next_row_ptr(row)
-
-			(row_ptr_min..row_ptr_max - 1).step(1) do |i|
-				a[row][@columns[i]] = @values[i]
-			end 
-		end
-		a
+		get_arrays
 	end
 
 	attr_reader :col_count
@@ -113,14 +111,68 @@ class SparseMatrix < Matrix
 		return 0
 	end
 
+	def get_row(i, default = nil)
+		row_ptr_min = @row_ptrs.fetch(i, nil)
+
+		if row_ptr_min == nil
+			if block_given?
+				return yield i
+			else
+				return default
+			end
+		end
+
+		a = Array.new(col_count, 0)
+		return a if row_ptr_min < 0
+
+		row_ptr_max = get_next_row_ptr(i)
+
+		(row_ptr_min...row_ptr_max).each do |j|
+			a[@columns[j]] = @values[j]
+		end 
+
+		a
+	end
+
 	def get_next_row_ptr(index)
+		return @values.size if index == row_count - 1
+
 		i = 1
-		next_ptr = @row_ptrs[index + i]
+		next_ptr = @row_ptrs.fetch(index + i, -1)
 		unless next_ptr >= 0
 			i += 1
 			next_ptr = @row_ptrs[index + i]
 		end
 		next_ptr
+	end
+
+	def get_arrays
+		a = []
+		row_count.times do |i|
+			if block_given?
+				a << yield(get_row(i))
+			else
+				a << get_row(i)
+			end
+		end
+		a
+	end
+
+	def self.create(row_count, column_count = row_count)	
+		values, columns, row_ptrs = [], [], []
+		row_count.times do |i|
+			first = -1
+			column_count.times do |j|
+				y = yield i, j
+				if y != 0
+					values << y
+					columns << j
+					first = values.size - 1 if first == -1
+				end
+			end
+			row_ptrs << first
+		end
+		new values, columns, row_ptrs, column_count
 	end
 
 end
