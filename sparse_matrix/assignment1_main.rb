45f272eb7c62095e603027be69311140b1df44c1
Andrew Fontaine <andrew@afontaine.ca>
2014-10-09 14:35:57 -0600

    working on assignment 1

--- sparse_matrix/assignment1_main.rb
+++ sparse_matrix/assignment1_main.rb
@@ -10,7 +10,18 @@ puts sparse.det
 puts sparse.inv
 puts sparse.diagonal?
 puts sparse * sparse == sparse**2
+puts sparse.lup.solve(Vector.elements(Array.new(30) { 1 }))
 
-tridiag = MatrixFactory.create(sparse.rows, false)
+tridiag = MatrixFactory.create(TridiagonalMatrix, sparse.to_a)
 
 puts tridiag == sparse
+puts sparse + tridiag
+puts tridiag.equal?(sparse)
+puts tridiag.solve(Vector.elements(Array.new(30) { 1 }))
+
+sparse = MatrixFactory.create(
+							  SparseMatrix,
+							  [[1, 0, 0, 0, 5], [0, 2, 0, 4, 0], [0, 0, 3, 0, 0], [0, 2, 0, 4, 0], [0, 0, 0, 0, 5]]
+							 )
+
+puts sparse.det
