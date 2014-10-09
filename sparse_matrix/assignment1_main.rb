6b438282cc54289d7a1793e8b4db0e13c3aa84f4
Andrew Fontaine <andrew@afontaine.ca>
2014-10-09 15:10:16 -0600

    This good bud?

--- sparse_matrix/assignment1_main.rb
+++ sparse_matrix/assignment1_main.rb
@@ -1,27 +1,70 @@
 require_relative 'sparse_matrix'
 
-sparse = MatrixFactory.build(SparseMatrix, 30) do |i, j|
-	next i * j + 1 if (i - 1.. i + 1) === j
-	0
-end
-
-puts sparse
-puts sparse.det
-puts sparse.inv
-puts sparse.diagonal?
-puts sparse * sparse == sparse**2
-puts sparse.lup.solve(Vector.elements(Array.new(30) { 1 }))
-
-tridiag = MatrixFactory.create(TridiagonalMatrix, sparse.to_a)
-
-puts tridiag == sparse
-puts sparse + tridiag
-puts tridiag.equal?(sparse)
-puts tridiag.solve(Vector.elements(Array.new(30) { 1 }))
-
+# Create is the same. TridiagonalMatrix will throw an exception if the matrix is not tridiagonal
 sparse = MatrixFactory.create(
 							  SparseMatrix,
 							  [[1, 0, 0, 0, 5], [0, 2, 0, 4, 0], [0, 0, 3, 0, 0], [0, 2, 0, 4, 0], [0, 0, 0, 0, 5]]
 							 )
 
-puts sparse.det
+# How to build different matricies
+sparse = MatrixFactory.build(SparseMatrix, 30) do |i, j|
+	next i * j + 1 if (i - 1.. i + 1) === j
+	0
+end
+
+matr = MatrixFactory.build(Matrix, 30) do |i, j|
+	next i * j + 1 if (i - 1.. i + 1) === j
+	0
+end
+
+tridiag = MatrixFactory.build(TridiagonalMatrix, 30) do |i, j|
+	i * j + 1
+end
+
+# Benchmarks
+require 'test/unit'
+require_relative './tests/sparse_benchmark_test'
+require_relative './tests/tridiagonal_matrix_benchmark'
+
+# Math
+tridiag + sparse
+
+sparse - tridiag
+
+sparse**2
+
+sparse * 2
+
+tridiag * sparse
+
+sparse / 2
+
+tridiag.det
+
+tridiag.inverse
+
+# etc
+
+
+# Properties
+tridiag.diagonal?
+
+sparse.square?
+
+tridiag.real?
+
+sparse.orthoganal?
+
+# etc.
+
+# Enumerate
+
+sparse.each(:nonzero) { |x| x }
+
+tridiag.each(:tridiagonal) { |x| x }
+
+sparse.each_with_index(:nonzero) { |x, i, j|  x + i + j }
+
+sparse.any? { |x| x != 0 }
+
+
