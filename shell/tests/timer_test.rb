0e9f0c1663c4fdd69cb909d28e8e13d72c945857
Andrew Fontaine <andrew@afontaine.ca>
2014-10-20 23:35:42 -0600

    Fixed a bunch of spelling errors
    * Had to rename time to timer, as time conflicts with a C function.
    * Renamed TimeCannotBeNegativeEror to TimeCannotBeNegativeError

--- shell/tests/timer_test.rb
+++ shell/tests/timer_test.rb
@@ -1,5 +1,5 @@
 require 'test/unit'
-require_relative '../timer'
+require_relative '../data/timer/timer'
 
 class TimerTest < Test::Unit::TestCase
   def setup
@@ -11,24 +11,24 @@ class TimerTest < Test::Unit::TestCase
   end
 
   def test_message
-    assert_equal('testing', Timer::time(5, 'testing'))
-    assert_equal('', Timer::time(7, ''))
+    assert_equal('testing', Timer::timer(1, 'testing'))
+    assert_equal('', Timer::timer(1, ''))
   end
 
   def test_time_limits
-    assert_raise(Timer::TimeTooLongError) { Timer::time((2**(0.size * 8 -2) -1) + 1, 'testing') }
-    assert_raise(Timer::TimeCannotBeNegativeEror) { Timer::time(-1, 'testing') }
+    assert_raise(Timer::TimeTooLongError) { Timer::timer((2**(0.size * 8 -2) -1) + 1, 'testing') }
+    assert_raise(Timer::TimeCannotBeNegativeError) { Timer::timer(-1, 'testing') }
   end
 
   def test_message_limits
-    assert_raise(Timer::MessageTooLongError) { Timer::time(8, 'a' * 256) }
-    assert_nothing_raised { Timer::time(10, '') }
+    assert_raise(Timer::MessageTooLongError) { Timer::timer(1, 'a' * 256) }
+    assert_nothing_raised { Timer::timer(1, '') }
   end
 
   def test_new_process
-    ruby = `ps | grep ruby | wc-l`.to_i
+    ruby = `ps | grep ruby | wc -l`.to_i
     Timer::start(100000000, "started")
-    assert_equal(ruby + 1, `ps | grep ruby | wc-l`.to_i)
+    assert_equal(ruby + 1, `ps | grep ruby | wc -l`.to_i)
   end
 
 end
