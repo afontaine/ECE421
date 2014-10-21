d5d6b90612a7f6fb26cb754d3e236cc4c60c5da4
Andrew Fontaine <andrew@afontaine.ca>
2014-10-20 22:45:45 -0600

    Gotta test some shiiiit

--- shell/data/timer/timer.c
+++ shell/data/timer/timer.c
@@ -4,20 +4,41 @@
 #include <unistd.h>
 #include <time.h>
 
-char* timer(int length, char* message) {
+static VALUE mTimer;
+static VALUE cTimeCannotBeNegative;
+static VALUE cTimeTooLongError;
+static VALUE cMessageTooLongError;
+
+VALUE timer(VALUE length, VALUE message) {
 	struct timespec len;
-	len.tv_sec = length;
+	len.tv_sec = FIX2INT(length);
 	len.tv_nsec = 0;
 	nanosleep(&len, NULL);
-	printf("%s", message);
+	printf("%s", StringValueCStr(message));
 	return message;
 }
 
-char* start(int length, char* message) {
+VALUE start(VALUE length, VALUE message) {
+	int len = rb_rescue(FIX2INT, length, rb_raise, cTimeTooLongError);
+	if(len < 0) {
+		rb_raise(cTimeCannotBeNegative, "Timer length cannot be negative");
+	}
+	if(RSTRING_LEN(message) > 255) {
+		rb_raise(cMessageTooLongError, "Message is longer than 255 characters");
+	}
 	pid_t proc;
 	proc = fork();
 	if(proc == 0) {
 		return timer(length, message);
 	}
-	exit(0);
+	return Qnil;
+}
+
+void Init_timer(void) {
+	mTimer = rb_define_module("Timer");
+	rb_define_module_function(mTimer, "timer", timer, 2);
+	rb_define_module_function(mTimer, "start", start, 2);
+	cTimeCannotBeNegative = rb_define_class_under(mTimer, "TimeCannotBeNegative", rb_eArgumentError);
+	cTimeTooLongError = rb_define_class_under(mTimer, "TimeTooLongError", rb_eArgumentError);
+	cMessageTooLongError = rb_define_class_under(mTimer, "MessageTooLongError", rb_eArgumentError);
 }
