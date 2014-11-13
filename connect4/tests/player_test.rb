df1fd357936fcca785f68dab941afbefd782b82e
Andrew Fontaine <andrew@afontaine.ca>
2014-11-12 21:11:10 -0700

    Fixed that bit

--- connect4/tests/player_test.rb
+++ connect4/tests/player_test.rb
@@ -3,7 +3,7 @@ require_relative '../data/model/player'
 
 class PlayerTest < Test::Unit::TestCase
   def test_player
-    andrew = Model::Player({X: 21}, [:X] * 4)
+    andrew = Model::Player.new({X: 21}, [:X] * 4)
     assert_equal({X: 21}, andrew.tokens)
     assert_equal([:X, :X, :X, :X], andrew.pattern)
   end
