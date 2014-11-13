76286d6a3426aa60582b5508a0524d9ce6e8ff05
Andrew Fontaine <andrew@afontaine.ca>
2014-11-13 14:33:05 -0700

    Added test for AI moves

--- connect4/tests/player_test.rb
+++ connect4/tests/player_test.rb
@@ -7,4 +7,11 @@ class PlayerTest < Test::Unit::TestCase
     assert_equal({X: 21}, andrew.tokens)
     assert_equal([:X, :X, :X, :X], andrew.pattern)
   end
+
+  def test_ai_move
+    game = Models::Board(6, 7)
+    player = Models::AIPlayer.new({O: 21}, [:O] * 4)
+    player.get_move(game)
+    assert_equal(20, player.tokens[:O])
+  end
 end
