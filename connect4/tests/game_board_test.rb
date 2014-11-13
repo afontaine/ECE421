c2fc913e3cbe05d8c791c755de5c4508c83e2700
Andrew Fontaine <andrew@afontaine.ca>
2014-11-13 11:16:56 -0700

    Did some error checking tests.

--- connect4/tests/game_board_test.rb
+++ connect4/tests/game_board_test.rb
@@ -8,7 +8,7 @@ class GameBoardTest < Test::Unit::TestCase
   def setup
     @andrew = Model::Player.new({X: 21}, [:X] * 4)
     @jacob = Model::Player.new({O: 21}, [:O] * 4)
-    @win = Model::GameBoard.new(2131155451212, 7)
+    @win = Model::GameBoard.new(6, 7)
     4.times { @win[4] = Model::GameToken.new(@andrew) }
     @one_more = Model::GameBoard.new(231454521, 789456)
     3.times { @one_more[4] = Model::GameToken.new(@jacob) }
@@ -27,5 +27,10 @@ class GameBoardTest < Test::Unit::TestCase
     assert_false(@one_more.win?(@andrew.pattern))
   end
 
-
+  def test_invalid_column
+    assert_raise(Model::IndexError) { @win[10] = Model::GameToken.new(@andrew) }
+    assert_raise(Model::FullColumnError) { 10.times { win[4] = Model::GameToken.new(@andrew) } }
+    assert_nothing_raised { @win[3] = Model::GameToken.new(@andrew) }
+    assert_nothing_raised { 7.times { @win[0] = Model::GameToken.new(@jacob) } }
+  end
 end
